1e150352b20c2d11037813973db1cc4bf3a3c24e
Verdial <99362544+wakeupver@users.noreply.github.com>
2026-04-16 03:19:34 +0700

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -145,30 +145,6 @@ for i in "${patch_files[@]}"; do
         echo "======================================"
         ;;
 
-    ## selinux/hooks.c
-    security/selinux/hooks.c)
-        echo "======================================"
-
-        if grep -q "security_secid_to_secctx" "security/selinux/hooks.c" >/dev/null 2>&1; then
-            echo "[-] Detected security_secid_to_secctx existed, security/selinux/hooks.c already patched!"
-        elif [ "$FIRST_VERSION" -lt 5 ] && [ "$SECOND_VERSION" -lt 10 ]; then
-            sed -i '/int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);/i\#ifdef CONFIG_KSU\n    static u32 ksu_sid;\n    char *secdata;\n#endif' security/selinux/hooks.c
-            sed -i '/if (!nnp && !nosuid)/i\#ifdef CONFIG_KSU\n    int error;\n    u32 seclen;\n#endif' security/selinux/hooks.c
-            sed -i '/return 0; \/\* No change in credentials \*\//a\\n#ifdef CONFIG_KSU\n    if (!ksu_sid)\n        security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);\n\n    error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);\n    if (!error) {\n        rc = strcmp("u:r:init:s0", secdata);\n        security_release_secctx(secdata, seclen);\n        if (rc == 0 && new_tsec->sid == ksu_sid)\n            return 0;\n    }\n#endif' security/selinux/hooks.c
-
-            if grep -q "security_secid_to_secctx" "security/selinux/hooks.c"; then
-                echo "[+] security/selinux/hooks.c Patched!"
-                echo "[+] Count: $(grep -c "security_secid_to_secctx" "security/selinux/hooks.c")"
-            else
-                echo "[-] security/selinux/hooks.c patch failed for unknown reasons, please provide feedback in time."
-            fi
-        else
-            echo "[-] Kernel does not need selinux fix, Skipped."
-        fi
-
-        echo "======================================"
-        ;;
-
     esac
 
 done
