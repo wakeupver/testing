7f0d1709f3cda5a2b059f8af53056ceb28ae2b7a
Verdial <99362544+wakeupver@users.noreply.github.com>
2026-06-01 14:35:16 +0700

    Remove SELinux hooks patching from patch.sh
    Removed SELinux hooks patching logic from the script.

--- patch.sh
+++ patch.sh
@@ -18,7 +18,6 @@ patch_files=(
     fs/read_write.c
     fs/stat.c
     kernel/reboot.c
-    security/selinux/hooks.c
 )
 
 for i in "${patch_files[@]}"; do
@@ -184,28 +183,6 @@ extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void 
         fi
         echo "======================================"
         ;;
-
-    ## selinux/hooks.c
-    security/selinux/hooks.c)
-        if grep -q "security_secid_to_secctx" "security/selinux/hooks.c" >/dev/null 2>&1; then
-            echo "[-] Detected security_secid_to_secctx existed, security/selinux/hooks.c Patched!"
-        elif [ "$FIRST_VERSION" -lt 5 ] && [ "$SECOND_VERSION" -lt 10 ]; then
-            sed -i '/int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);/i\#ifdef CONFIG_KSU\n    static u32 ksu_sid;\n    char *secdata;\n#endif' security/selinux/hooks.c
-            sed -i '/if (!nnp && !nosuid)/i\#ifdef CONFIG_KSU\n    int error;\n    u32 seclen;\n#endif' security/selinux/hooks.c
-            sed -i '/return 0; \/\* No change in credentials \*\//a\\n#ifdef CONFIG_KSU\n    if (!ksu_sid)\n        security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);\n\n    error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);\n    if (!error) {\n        rc = strcmp("u:r:init:s0", secdata);\n        security_release_secctx(secdata, seclen);\n        if (rc == 0 && new_tsec->sid == ksu_sid)\n            return 0;\n    }\n#endif' security/selinux/hooks.c
-
-            if grep -q "security_secid_to_secctx" "security/selinux/hooks.c"; then
-                echo "[+] security/selinux/hooks.c Patched!"
-                echo "[+] Count: $(grep -c "security_secid_to_secctx" "security/selinux/hooks.c")"
-            else
-                echo "[-] security/selinux/hooks.c patch failed for unknown reasons, please provide feedback in time."
-            fi
-        else
-            echo "[-] Kernel needn't selinux fix, Skipped."
-        fi
-
-        echo "======================================"
-        ;;        
 
     esac
 
